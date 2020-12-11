e2ab451ea383ed40d0cb0abd6c292941e35b9818
alestor123 <alestoraldous@gmail.com>
2020-12-11 17:11:49 +0530

    added validation

--- App.js
+++ App.js
@@ -3,8 +3,15 @@
 var Box = require("cli-box"),
 figlet = require('figlet'),
 pck = require('./package.json'),
-christmasDate = new Date(`Dec 25, ${new Date().getFullYear()} 00:00:00`).getTime(),
+christmasDate = new Date(`Dec 25, ${new Date().getFullYear()} 24:00:00`).toJSON().slice(0,10),
 chalk = require('chalk'); 
-figlet('Merry Christmas!!', (err, data) => {
-if (err) throw err     
-console.log(Box('90x10',{text: chalk.green(data+`\n By ${process.argv[2]||pck.author||'alestor123' }`)}).toString())})
+if(new Date().toJSON().slice(0,10)===christmasDate){
+    figlet('Merry Christmas!!', (err, data) => {
+        if (err) throw err     
+        console.log(Box('90x10',{text: chalk.green(data+`\n By ${process.argv[2]||pck.author||'alestor123' }`)}).toString())})
+}
+else{
+    figlet('Christmas Is Over :(', (err, data) => {
+        if (err) throw err     
+        console.log(data)
+})}

274e4457777085701f19ebb3adf0cfd006c3f497
alestor123 <alestoraldous@gmail.com>
2020-12-11 17:02:23 +0530

    working on christmas validation

--- App.js
+++ App.js
@@ -3,7 +3,8 @@
 var Box = require("cli-box"),
 figlet = require('figlet'),
 pck = require('./package.json'),
-chalk = require('chalk');
+christmasDate = new Date(`Dec 25, ${new Date().getFullYear()} 00:00:00`).getTime(),
+chalk = require('chalk'); 
 figlet('Merry Christmas!!', (err, data) => {
 if (err) throw err     
 console.log(Box('90x10',{text: chalk.green(data+`\n By ${process.argv[2]||pck.author||'alestor123' }`)}).toString())})

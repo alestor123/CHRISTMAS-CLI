1273fc6a27e826c01090a9d5887cd38e12adf037
alestor123 <alestoraldous@gmail.com>
2020-12-11 14:10:09 +0530

    almost done

--- App.js
+++ App.js
@@ -5,6 +5,5 @@ figlet = require('figlet'),
 pck = require('./package.json'),
 chalk = require('chalk');
 figlet('Merry Christmas!!', (err, data) => {
-    if (err) throw err     
-console.log(Box('90x10',{text: chalk.green(data+`\n By ${process.argv[2]||pck.author }`) , vAlign: "center"
-, hAlign: "center"}).toString())})
+if (err) throw err     
+console.log(Box('90x10',{text: chalk.green(data+`\n By ${process.argv[2]||pck.author||'alestor123' }`)}).toString())})

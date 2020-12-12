240360cf3fdb86402560b4546ac676581b66514c
alestor123 <alestoraldous@gmail.com>
2020-12-12 16:43:40 +0530

    added new year comming soon

--- App.js
+++ App.js
@@ -2,7 +2,6 @@
 
 var Box = require("cli-box"),
 figlet = require('figlet'),
-pck = require('./package.json'),
 christmasDate = new Date(`Dec 26`).toJSON().slice(5,10),
 newyearDate = new Date('Jan 2').toJSON().slice(5,10),
 now = new Date().toJSON().slice(5,10),
@@ -13,13 +12,20 @@ if(now===christmasDate){
 else if(newyearDate===now){
     wish('Happy New Year')
 }
+else if(now > christmasDate){
+    wait('New Year Is Comming !!')
+}
 else{
-    figlet('Christmas Is Coming Soon : )', (err, data) => {
-        if (err) throw err     
-        console.log(chalk.red.bgWhite.bold(data))
-})}
+    wait('Christmas Is Coming Soon : )')
+}
 function wish(wishes){
     figlet(wishes, (err, data) => {
         if (err) throw err     
         console.log(chalk.bgWhite.bold(Box('90x10',{text: chalk.green(data+`\n By ${process.argv[2]||'alestor123' }`)}).toString()))})
 }
+function wait(waitmsg){
+    figlet(waitmsg, (err, data) => {
+        if (err) throw err     
+        console.log(chalk.red.bgWhite.bold(data))
+
+})}

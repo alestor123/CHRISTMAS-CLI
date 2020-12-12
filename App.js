341635ae2079d6e0086c1e7cc68d8c3b910b649f
alestor123 <alestoraldous@gmail.com>
2020-12-12 16:05:20 +0530

    added a simple new year wish proto code

--- App.js
+++ App.js
@@ -3,15 +3,23 @@
 var Box = require("cli-box"),
 figlet = require('figlet'),
 pck = require('./package.json'),
-christmasDate = new Date(`Dec 25, ${new Date().getFullYear()} 24:00:00`).toJSON().slice(0,10),
+christmasDate = new Date(`Dec 26`).toJSON().slice(5,10),
+newyearDate = new Date('Jan 2').toJSON().slice(5,10),
+now = new Date().toJSON().slice(5,10),
 chalk = require('chalk'); 
-if(new Date().toJSON().slice(0,10)===christmasDate){
-    figlet('Merry Christmas!!', (err, data) => {
-        if (err) throw err     
-        console.log(chalk.bgWhite.bold(Box('90x10',{text: chalk.green(data+`\n By ${process.argv[2]||pck.author||'alestor123' }`)}).toString()))})
+if(now===christmasDate){
+    wish('Merry Christmas')
+}
+else if(newyearDate===now){
+    wish('Happy New Year')
 }
 else{
     figlet('Christmas Is Coming Soon : )', (err, data) => {
         if (err) throw err     
         console.log(chalk.red.bgWhite.bold(data))
 })}
+function wish(wishes){
+    figlet(wishes, (err, data) => {
+        if (err) throw err     
+        console.log(chalk.bgWhite.bold(Box('90x10',{text: chalk.green(data+`\n By ${process.argv[2]||pck.author||'alestor123' }`)}).toString()))})
+}
